daeddbabb3a09c3c4d12c01ebc97a0d3dc3edb39
mkarmark <mikarmar@microsoft.com>
2024-10-08 17:27:49 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,5 +2,8 @@ module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: { text: "Hello from the API" },
+    headers: {
+            "Content-Length": Buffer.byteLength(responseMessage, 'utf8')
+        }
   };
 };
