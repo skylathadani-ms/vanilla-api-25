325393fda617f2937e7b8978cae6eaa2060836b8
Skyla Thadani <128430081+skylathadani-ms@users.noreply.github.com>
2024-11-22 11:39:22 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-  const responseMessage = {"Hello, this is your response! 10/23/24 10:17pm"};
+  const responseMessage = {"greeting": "Hello, this is your response! 10/23/24 10:17pm"};
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage,
