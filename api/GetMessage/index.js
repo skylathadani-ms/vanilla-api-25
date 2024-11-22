fcf73d7bcb366f55746863a155e374c3330b9382
Skyla Thadani <128430081+skylathadani-ms@users.noreply.github.com>
2024-11-22 11:43:15 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,10 +1,14 @@
 module.exports = async function (context, req) {
-  const responseMessage = "Hello, this is your response! 10/23/24 10:17pm";
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage,
-        headers: {
-            "Content-Length": Buffer.byteLength(responseMessage, 'utf8')
-        }
-    };
+  const responseMessage = {
+    message: "Hello, this is your response! 10/23/24 10:17pm"
+  };
+  context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: JSON.stringify(responseMessage),
+      headers: {
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(JSON.stringify(responseMessage), 'utf8')
+      }
+  };
 };
+
